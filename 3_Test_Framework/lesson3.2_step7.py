a00c5bbcc63887b813722df3219e0653d290e11b
Git-Sergio <sergii.grushkovyk@gmail.com>
2019-08-02 15:48:19 +0200

    Home work

--- 3_Test_Framework/lesson3.2_step7.py
+++ 3_Test_Framework/lesson3.2_step7.py
@@ -42,10 +42,9 @@ assert catalog_text == "Каталог", \
     "Wrong language, got" + catalog_text + "instead of 'Каталог'" 
 
 
-
-
-
-
+# "Let's count together! {}, then goes {}, and then {}".format("one", "two", "three")
+# f"Wrong text, got {actual_result}, something wrong"  #важно! версия python >= 3.6
+#Лучше использовать f-string вместо .format()
 
 # f"Wrong text, got {actual_result}, something wrong"  
 # важно! версия python >= 3.6
---
+++ Home_Work/test_abs_project.py
@@ -0,0 +1,4 @@
+# "Let's count together! {}, then goes {}, and then {}".format("one", "two", "three")
+# f"Wrong text, got {actual_result}, something wrong"  #важно! версия python >= 3.6
+#Лучше использовать f-string вместо .format()
+
